dfdb7001b6a0de0fa40d366147e03e318ba89b87
Mary Mettille <mary.mettille@startribune.com>
2025-08-01 12:42:37 -0500

    Try building with vite to bundle css with the js ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 // This is what we will export out to the public.
+import "./tailwind.css"
 export * from './components';
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
-  css: {
-    postcss: './postcss.config.js',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
   },
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin(),],
   build: {
     lib: {
-      entry: ('src/index.ts'),
+      entry: 'src/index.ts',
+      formats: ['es'],
       name: 'DesignSystem',
       fileName: (format) => `design-system.${format}.js`,
-      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -22,5 +23,11 @@ export default defineConfig({
         },
       },
     },
+    target: 'esnext',
+    sourcemap: true,
+  },
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'react',
   },
 });
