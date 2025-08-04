49cdc4e6f2f9e5141e805106bce6d4c4b0eba362
Mary Mettille <mary.mettille@startribune.com>
2025-08-04 12:06:29 -0500

    Try again to minify the css

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { execSync } from 'child_process';
 
 export default defineConfig({
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
   },
-  plugins: [react(), cssInjectedByJsPlugin(),],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+    {
+      name: 'postcss-build',
+      closeBundle() {
+        execSync(
+          'cross-env NODE_ENV=production tailwindcss -i ./src/tailwind.css -o ./dist/design-system.css --minify',
+          { stdio: 'inherit' }
+        );
+      }
+    }
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
