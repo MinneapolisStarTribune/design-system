68f8f977cb39e2e6055b42dcc1d19985c510dfdb
Mary Mettille <mary.mettille@startribune.com>
2025-11-28 13:57:48 -0600

    Add breakpoint tokens to UI Library

--- scripts/build-tokens.js
+++ scripts/build-tokens.js
@@ -28,6 +28,8 @@ function getStyleDictionaryConfig(brand) {
       'tokens/color/semantic.json',
       'tokens/text.json',
       'tokens/border-radius.json',
+      'tokens/spacing.json',
+      'tokens/breakpoint.json',
     ],
     hooks: {
       formats: {
