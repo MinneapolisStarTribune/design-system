d1704c3f217d5dfcb929fadb3194313fb56208b7
Mary Mettille <mary.mettille@startribune.com>
2026-01-06 23:30:54 -0600

    Merge with main

--- src/components/Button/button.stories.tsx
+++ src/components/Button/button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { Button, BUTTON_VARIANTS, BUTTON_COLORS, BUTTON_SIZES } from './Button';
 
