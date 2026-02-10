034629415249516c17784ba1d3f6ae0e6af813f4
Yuvaraj Pattabi NLT <yuvaraj.pattabi@startribune.com>
2026-02-10 17:51:25 +0530

    relocate EditorialText component to the Editorial folder

--- src/components/Typography/Editorial/EditorialText/EditorialText.stories.tsx
+++ src/components/Typography/Editorial/EditorialText/EditorialText.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react-vite';
-import { FontWeight, SIZES } from '../../../types/globalTypes';
+import { FontWeight, SIZES } from '../../../../types/globalTypes';
 import { EditorialText } from './EditorialText';
 
 const meta = {
