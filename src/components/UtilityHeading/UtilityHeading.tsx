1b396c0df1f725ed19f2d72ea7c1a506862bcd4e
Adam Hay <46979804+hayadam88@users.noreply.github.com>
2026-02-11 09:27:31 -0600

    adds some clarifying comments

--- src/components/UtilityHeading/UtilityHeading.tsx
+++ src/components/UtilityHeading/UtilityHeading.tsx
@@ -5,7 +5,8 @@ export interface UtilityHeadingProps extends BaseProps {
   importance: number;
   children: React.ReactNode;
   //This would be either 'section' or 'page', but one issue is page is used for both Varsity and Strib
-  //To expand on that, a heading with importance 1 is title the same thing for both Varisty and Strib: utility-page-h1
+  //To expand on that, a heading with importance 1 is titled the same thing for both Varisty and Strib: utility-page-h1
+  //also, do we even want to deal with this as a prop? How else would we handle it otherwise?
   headingType: string;
 }
 
@@ -15,11 +16,11 @@ export const UtilityHeading: React.FC<UtilityHeadingProps> = ({
   children,
   dataTestId,
 }) => {
-  // Generate heading class name based on props
-  // Pattern: typography-{headingType}-label-{weight}-{size}[-caps]
+  //Generate heading class name based on props
   const typographyClassName = `typography-utility-${headingType}-h${importance}`;
 
   return (
+    // I wasn't sure what tag to use here. Should we be doing h1's, h2's, etc., instead? Something else?
     <span className={typographyClassName} data-testid={dataTestId}>
       {children}
     </span>
