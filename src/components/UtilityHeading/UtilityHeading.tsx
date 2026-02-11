e6dc6a40473c3cf0e0ff5a61b3cc5a363d6f7030
Adam Hay <46979804+hayadam88@users.noreply.github.com>
2026-02-11 14:11:30 -0600

    refactors code

--- src/components/UtilityHeading/UtilityHeading.tsx
+++ src/components/UtilityHeading/UtilityHeading.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 import { BaseProps } from '../../types/globalTypes';
 
 export interface UtilityHeadingProps extends BaseProps {
   importance: number;
   children: React.ReactNode;
-  //This would be either 'section' or 'page', but one issue is page is used for both Varsity and Strib
-  //To expand on that, a heading with importance 1 is titled the same thing for both Varisty and Strib: utility-page-h1
-  //also, do we even want to deal with this as a prop? How else would we handle it otherwise?
+  // 'section' or 'page'
   headingType: string;
+  className?: string;
 }
 
 export const UtilityHeading: React.FC<UtilityHeadingProps> = ({
@@ -15,14 +15,19 @@ export const UtilityHeading: React.FC<UtilityHeadingProps> = ({
   headingType,
   children,
   dataTestId,
+  className,
 }) => {
   //Generate heading class name based on props
   const typographyClassName = `typography-utility-${headingType}-h${importance}`;
+  const combinedClassNames = classNames(typographyClassName, className);
+  const HeadingTag = `h${importance}`;
 
-  return (
-    // I wasn't sure what tag to use here. Should we be doing h1's, h2's, etc., instead? Something else?
-    <span className={typographyClassName} data-testid={dataTestId}>
-      {children}
-    </span>
+  return React.createElement(
+    HeadingTag,
+    {
+      className: combinedClassNames,
+      'data-testid': dataTestId,
+    },
+    children
   );
 };
