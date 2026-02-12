990a5ad4f84eabf93a00044d06fa5d21337eafe0
Mary Mettille <mary.mettille@startribune.com>
2026-02-12 14:48:15 -0600

    feat: SUS-144 - Create FormControl compound component

---
+++ src/components/FormControl/FormControl.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BaseProps } from '../../types/globalTypes';
+import { TextInput } from './TextInput/TextInput';
+
+export interface FormControlProps extends BaseProps {
+  isDisabled?: boolean;
+}
+
+export const FormControl: React.FC<FormControlProps> & {
+  TextInput: React.ComponentType<any>;
+  // Add any new form control subcomponents here
+} = () => {
+  if (typeof console !== 'undefined' && console.error) {
+    console.error(
+      'FormControl must be used with a subcomponent (e.g., FormControl.TextInput). ' +
+        'Use <FormControl.TextInput /> instead of <FormControl />.'
+    );
+  }
+  return null;
+};
+
+// Attach subcomponents
+FormControl.TextInput = TextInput;
+// Add any new form control subcomponents here
