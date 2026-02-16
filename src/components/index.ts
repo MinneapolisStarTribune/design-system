b693f999e24ae4e0d53526978ed2d67e1f966aab
Yuvaraj Pattabi NLT <yuvaraj.pattabi@startribune.com>
2026-02-16 16:05:38 +0530

    feat: add ArticleBodyText component with accessibility tests and Storybook stories

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@
 export * from './Button/Button';
 export * from './Icon/Icon';
 //Typography Components
-export * from './Typography/editorial';
-export * from './Typography/utility';
+export * from './Typography/Editorial';
+export * from './Typography/Utility';
 export * from './Typography/NewsHeading/NewsHeading';
+export * from './Typography/ArticleBody';
