0f5e08f70317135a1aa9b7e091462572d4dbff60
Susie <susie.douang@startribune.com>
2026-02-17 09:07:55 -0600

    add build-info.ts, generated only when theme output changes

--- src/generated/build-info.ts
+++ src/generated/build-info.ts
@@ -3,6 +3,6 @@
  * Build time and package version for this design-system build.
  */
 export const buildInfo = {
-  buildTime: '2026-02-17T15:03:43.706Z',
+  buildTime: '2026-02-17T15:05:54.493Z',
   version: '0.0.7',
 } as const;
