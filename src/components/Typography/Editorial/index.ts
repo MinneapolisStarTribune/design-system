1cf959f13721940e0ef5cabb8cacbee044816c7d
Yuvaraj Pattabi NLT <yuvaraj.pattabi@startribune.com>
2026-02-17 15:59:38 +0530

    feat: export EditorialSponsoredText component from Typography index

--- src/components/Typography/Editorial/index.ts
+++ src/components/Typography/Editorial/index.ts
@@ -3,12 +3,7 @@
  * Use createEditorialHeading to define new heading variants (NewsHeading, etc.).
  */
 export { createEditorialHeading } from './createEditorialHeading';
-export { EnterpriseHeading } from './EnterpriseHeading/EnterpriseHeading';
-export type {
-  CreateEditorialHeadingOptions,
-  EditorialHeadingImportance,
-  EditorialHeadingProps,
-} from './types';
+export type * from './types';
 
 export * from './EditorialText/EditorialText';
 export * from './EditorialSponsoredText/EditorialSponsoredText';
