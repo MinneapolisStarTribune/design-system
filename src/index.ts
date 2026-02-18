dcfc3dc27c607e47f86463a04b647cf2c64c6bd0
Mary Mettille <mary.mettille@startribune.com>
2026-02-18 12:31:28 -0600

    Sort index.ts alphabetically by import path

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// This is what we will export out to the public.
+// This is what we will export out to consuming apps. This file is sorted alphabetically.
 export * from './components';
-export { DesignSystemProvider, type Brand } from './providers/MantineProvider';
+export { type Brand, DesignSystemProvider } from './providers/MantineProvider';
 export { getBrandFontPath, loadBrandFonts } from './styles/fonts';
