51f792396f8eefc57b55b7113f8143d37bb37ff9
Mary Mettille <mary.mettille@startribune.com>
2026-02-23 22:46:31 -0600

    Add platform-specific entry points

---
+++ src/index.native.ts
@@ -0,0 +1,7 @@
+// This is what we will export out to consuming apps for react native apps. This file is sorted alphabetically.
+
+// Export components (will be populated in Ticket 4)
+export * from './components/index.native';
+
+// Export Brand type for now (DesignSystemProvider will be adapted for React Native in a later ticket)
+export type { Brand } from './providers/MantineProvider';
