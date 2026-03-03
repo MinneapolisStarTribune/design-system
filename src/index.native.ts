a586755e9fbad6c64579d436ae16f5e78e239364
Mary Mettille <mary.mettille@startribune.com>
2026-03-03 01:18:00 -0600

    Export TamaguiProvider instead of mantiine

--- src/index.native.ts
+++ src/index.native.ts
@@ -4,4 +4,4 @@
 export * from './components/index.native';
 
 // Export Brand type for now (DesignSystemProvider will be adapted for React Native in a later ticket)
-export type { Brand } from './providers/MantineProvider';
+export { type Brand, DesignSystemProvider } from './providers/TamaguiProvider';
--- src/index.web.ts
+++ src/index.web.ts
@@ -1,5 +1,5 @@
 // This is what we will export out to consuming apps for the web. This file is sorted alphabetically.
 // Import from index.ts (which re-exports from index.web.ts) for DTS plugin compatibility
 export * from './components';
-export { type Brand, DesignSystemProvider } from './providers/MantineProvider';
+export { type Brand, DesignSystemProvider } from './providers/TamaguiProvider';
 export { getBrandFontPath, loadBrandFonts } from './styles/fonts';
