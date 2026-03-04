63974b0bf52e4c4545919235b015c4c1acef9e6d
Adam Hay <46979804+hayadam88@users.noreply.github.com>
2026-03-04 17:31:16 -0600

    refactors old  styling into scss file and adds more styling

--- src/components/FormGroup/FormGroup.Description.tsx
+++ src/components/FormGroup/FormGroup.Description.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BaseProps } from '@/types/globalTypes';
 import { useFormGroupContext } from './FormGroupContext';
+import styles from './FormGroup.module.scss';
 
 export interface FormGroupDescriptionProps extends BaseProps {
   children: React.ReactNode;
@@ -19,12 +20,7 @@ export const FormGroupDescription: React.FC<FormGroupDescriptionProps> = ({
     <span
       id={id}
       data-testid={dataTestId}
-      className="typography-utility-text-regular-x-small"
-      style={{
-        display: 'block',
-        marginTop: 'var(--space-2, 2px)',
-        marginBottom: 'var(--space-8, 8px)',
-      }}
+      className={`typography-utility-text-regular-x-small ${styles.description}`}
     >
       {children}
     </span>
--- src/components/FormGroup/FormGroup.Label.tsx
+++ src/components/FormGroup/FormGroup.Label.tsx
@@ -2,6 +2,7 @@ import { BaseProps } from '@/types/globalTypes';
 import { useFormGroupContext } from './FormGroupContext';
 import { UtilityBody } from '../Typography/Utility/UtilityBody/UtilityBody';
 import { UtilityLabel } from '../Typography/Utility/UtilityLabel/UtilityLabel';
+import styles from './FormGroup.module.scss';
 
 export interface FormGroupLabelProps extends BaseProps {
   children: React.ReactNode;
@@ -23,7 +24,7 @@ export const FormGroupLabel: React.FC<FormGroupLabelProps> = ({
   const htmlForValue = htmlFor ?? context?.inputId;
 
   return (
-    <div style={{ display: 'flex', alignItems: 'baseline', flexWrap: 'wrap', gap: '4px' }}>
+    <div className={styles.label}>
       <UtilityLabel
         id={id}
         htmlFor={htmlForValue}
@@ -39,7 +40,7 @@ export const FormGroupLabel: React.FC<FormGroupLabelProps> = ({
           // size and weight are always the same
           size="xx-small"
           weight="regular"
-          style={{ margin: 0 }}
+          className={styles.optional}
         >
           <div style={{ color: 'var(--color-text-on-light-tertiary)' }}>(Optional)</div>
         </UtilityBody>
