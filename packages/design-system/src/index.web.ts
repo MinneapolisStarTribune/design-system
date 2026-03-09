23e1175bead576569a0db90822a9413add819f54
Mary Mettille <mary.mettille@startribune.com>
2026-03-09 01:42:05 -0500

    Lint and Test Cruft

--- packages/design-system/src/index.web.ts
+++ packages/design-system/src/index.web.ts
@@ -1,6 +1,6 @@
 // Web entrypoint for consuming apps. Import from '@minneapolisstartribune/design-system/web'.
 // This file is sorted alphabetically.
 export * from '@/components/index.web';
-export { type Brand, DesignSystemProvider } from '@/providers/DesignSystemProvider';
 export { AnalyticsProvider } from '@/providers/AnalyticsProvider';
+export { type Brand, DesignSystemProvider } from '@/providers/DesignSystemProvider';
 export { getBrandFontPath, loadBrandFonts } from '@/styles/fonts';
