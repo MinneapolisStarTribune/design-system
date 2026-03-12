6e36937fa6a83f28bad90ae6eb6201d2c0849db1
Sathish Kumar <Sathish.Kumar@startribune.com>
2026-03-12 19:28:54 +0530

    Updated story

--- packages/design-system/src/components/Swiper/web/Swiper.stories.tsx
+++ packages/design-system/src/components/Swiper/web/Swiper.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { SwiperCarousel } from './SwiperCarousel';
 
 const meta: Meta<typeof SwiperCarousel> = {
