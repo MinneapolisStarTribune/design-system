7c10fa4c3201118528fdda172f37094efc0e0a5c
Yuvaraj Pattabi NLT <yuvaraj.pattabi@startribune.com>
2026-03-13 16:18:47 +0530

    feat: add EnhancedCodeBlock with size and variant support

---
+++ packages/design-system/src/components/EditorialContent/ArticleToolkit/CodeBlock/EnhancedCodeBlock/web/EnhancedCodeBlock.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+import { DangerousCodeBlock } from '../../DangerousCodeBlock/web/DangerousCodeBlock';
+import styles from './EnhancedCodeBlock.module.scss';
+import type { EnhancedCodeBlockProps } from '../EnhancedCodeBlock.types';
+
+/**
+ * EnhancedCodeBlock wraps `DangerousCodeBlock` and applies `size` × `variant` styles
+ * specific to enhanced code block layouts.
+ */
+export const EnhancedCodeBlock: React.FC<EnhancedCodeBlockProps> = ({
+  size = 'full',
+  variant = 'standard',
+  cleanQuotes = false,
+  ...rest
+}) => {
+  return (
+    <DangerousCodeBlock
+      className={classNames(
+        styles['enhanced-code-block'],
+        styles[`variant-${variant}`],
+        styles[`size-${size}`]
+      )}
+      variant={variant}
+      size={size}
+      cleanQuotes={cleanQuotes}
+      {...rest}
+    />
+  );
+};
+
+EnhancedCodeBlock.displayName = 'EnhancedCodeBlock';
