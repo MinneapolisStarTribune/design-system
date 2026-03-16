4bf141c691db3cae953a93e33d5abfc0cf3dcc0b
SathishKumarRNLT <Sathish.Kumar@startribune.com>
2026-03-16 08:53:52 +0000

    Refactored and renamed

--- packages/design-system/src/components/SwiperCarousel/web/SwiperCarousel.stories.tsx
+++ packages/design-system/src/components/SwiperCarousel/web/SwiperCarousel.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { SwiperCarousel } from './SwiperCarousel';
-import React from 'react';
 
 const meta: Meta<typeof SwiperCarousel> = {
   title: 'Components/SwiperCarousel',
