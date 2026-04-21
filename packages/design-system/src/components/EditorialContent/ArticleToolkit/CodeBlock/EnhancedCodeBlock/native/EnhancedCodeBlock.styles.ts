284d64e0473f1b41f894e3473a17d4cecf986c59
Yuvaraj Pattabi NLT <yuvaraj.pattabi@startribune.com>
2026-04-21 11:55:49 +0530

    replace hardcoded max-width values with theme-based variables in DangerousCodeBlock and EnhancedCodeBlock styles

--- packages/design-system/src/components/EditorialContent/ArticleToolkit/CodeBlock/EnhancedCodeBlock/native/EnhancedCodeBlock.styles.ts
+++ packages/design-system/src/components/EditorialContent/ArticleToolkit/CodeBlock/EnhancedCodeBlock/native/EnhancedCodeBlock.styles.ts
@@ -14,12 +14,26 @@ type Styles = {
 export const createStyles = (theme: NativeTheme) =>
   StyleSheet.create<Styles>({
     container: {},
-    'variant-standard-size-full': { maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile },
-    'variant-standard-size-large': { maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile },
-    'variant-standard-size-medium': { maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile },
-    'variant-standard-size-inline': { maxWidth: theme.semanticArticleToolkitMaxWidthStandardInlineMobile },
+    'variant-standard-size-full': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile,
+    },
+    'variant-standard-size-large': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile,
+    },
+    'variant-standard-size-medium': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthStandardFullMobile,
+    },
+    'variant-standard-size-inline': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthStandardInlineMobile,
+    },
     'variant-immersive-size-full': { width: '100%', maxWidth: undefined },
-    'variant-immersive-size-large': { maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveLargeMobile },
-    'variant-immersive-size-medium': { maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveMediumMobile },
-    'variant-immersive-size-inline': { maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveInlineMobile },
+    'variant-immersive-size-large': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveLargeMobile,
+    },
+    'variant-immersive-size-medium': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveMediumMobile,
+    },
+    'variant-immersive-size-inline': {
+      maxWidth: theme.semanticArticleToolkitMaxWidthImmersiveInlineMobile,
+    },
   });
